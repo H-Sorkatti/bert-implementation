063b29e7a5c7e6acf974c16b4399c35aeb35817b
H-Sorkatti <H.Sorkatti.B@Gmail.com>
2024-09-10 19:40:12 +0300

    Added config class

--- Bert.py
+++ Bert.py
@@ -42,11 +42,11 @@ class AttentionHead(nn.Module):
     Impelements Dot-Product Self-Attention
     """
 
-    def __init__(self, config):
+    def __init__(self, config, head_dim):
         super().__init__()
-        self.q_net = nn.Linear(config.embed_dim, config.head_dim, bias=False)
-        self.k_net = nn.Linear(config.embed_dim, config.head_dim, bias=False)
-        self.v_net = nn.Linear(config.embed_dim, config.head_dim, bias=False)
+        self.q_net = nn.Linear(config.embed_dim, head_dim, bias=False)
+        self.k_net = nn.Linear(config.embed_dim, head_dim, bias=False)
+        self.v_net = nn.Linear(config.embed_dim, head_dim, bias=False)
 
     def forward(self, input_ids):
         q = self.q_net(input_ids)
@@ -65,10 +65,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.head_dim = config.embed_dim // config.num_heads
         self.heads = nn.Sequential(
-            *[
-                AttentionHead(config.embed_dim, self.head_dim)
-                for _ in range(config.num_heads)
-            ]
+            *[AttentionHead(config, self.head_dim) for _ in range(config.num_heads)]
         )
 
     def forward(self, input_ids):
@@ -81,10 +78,10 @@ class FeedForward(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.linear_1 = nn.Linear(
-            in_features=config.input_dim, out_features=config.intermediate_dim
+            in_features=config.embed_dim, out_features=config.intermediate_dim
         )
         self.linear_2 = nn.Linear(
-            in_features=config.intermediate_dim, out_features=config.input_dim
+            in_features=config.intermediate_dim, out_features=config.embed_dim
         )
         self.gelu = nn.GELU()
         self.dropout = nn.Dropout(0.1)
@@ -100,12 +97,10 @@ class FeedForward(nn.Module):
 class TransformerEncoderLayer(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.layernorm_1 = nn.LayerNorm(config.input_dim)
-        self.layernorm_2 = nn.LayerNorm(config.input_dim)
-        self.multihead_attention = MultiHeadAttention(
-            config.input_dim, config.num_heads
-        )
-        self.feed_forward = FeedForward(config.input_dim, config.intermediate_dim)
+        self.layernorm_1 = nn.LayerNorm(config.embed_dim)
+        self.layernorm_2 = nn.LayerNorm(config.embed_dim)
+        self.multihead_attention = MultiHeadAttention(config)
+        self.feed_forward = FeedForward(config)
 
     def forward(self, input_ids):
         x = self.layernorm_1(input_ids)
@@ -120,19 +115,69 @@ class TransformerEncoderLayer(nn.Module):
 class TransformerEncoder(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.embeddings = Embeddings(
-            config.embed_dim, config.vocab_size, config.max_seq_length
-        )
+        self.embeddings = Embeddings(config)
         self.encoder_layers = nn.Sequential(
-            *[
-                TransformerEncoderLayer(
-                    config.embed_dim, config.num_heads, config.intermediate_dim
-                )
-                for _ in range(config.num_encoder_blocks)
-            ]
+            *[TransformerEncoderLayer(config) for _ in range(config.num_encoder_blocks)]
         )
 
     def forward(self, input_ids):
         embeddings = self.embeddings(input_ids)
         x = self.encoder_layers(embeddings)
+        return x
+
+
+class BertConfig:
+    def __init__(
+        self,
+        embed_dim=768,
+        vocab_size=30_000,
+        max_seq_length=50,
+        num_encoder_blocks=12,
+        num_heads=8,
+        intermediate_dim=3072,
+        classification_head=False,
+        num_labels=None
+    ):
+        self.embed_dim = embed_dim
+        self.vocab_size = vocab_size
+        self.max_seq_length = max_seq_length
+        self.num_encoder_blocks = num_encoder_blocks
+        self.num_heads = num_heads
+        self.intermediate_dim = intermediate_dim
+        self.classification_head = classification_head
+        self.num_labels = num_labels
+
+
+class BERT(nn.Module):
+    """
+    This is an implementation of BERT (Bidirectional Encoder Representation) architecture.
+
+    parameters:
+        config:  The model configuration. Must be a BertConfig object that contains:
+                    embed_dim: Inner-model hidden-state dimension.
+                    vocab_size: Vocabulary size of the used dataset.
+                    max_seq_length: Maximum sequence length.
+                    num_encoder_blocks: Number of encoder blocks.
+                    num_heads: Number of heads in the Self-Attention layer.
+                    intermediate_dim: Dimension of the inner feed-forward neural network.
+                    classification_head: bool. Default 'False'.
+                    num_labels: Number of labels for the classification head.
+                    
+    methods:
+        forward: Takes a torch tensor of int token IDs of shape (N, max_seq_length), where N is the batch size.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.encoder = TransformerEncoder(config)
+        if self.config.classification_head:
+            self.dropout = nn.Dropout()
+            self.classifier = nn.Linear(config.embed_dim, config.num_labels)
+
+    def forward(self, input_ids):
+        x = self.encoder(input_ids)
+        if self.config.classification_head:
+            x = self.dropout(x[:, 0, :])
+            x = self.classifier(x)
         return x
